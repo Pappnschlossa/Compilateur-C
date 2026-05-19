ecc0079e50ee1ed2895a8bcbbb5dbec3e5bcc690
Marie Choquet <marie.choquet5@etu.univ-lorraine.fr>
2026-05-19 11:38:22 +0200

    All in terminal

--- main.py
+++ main.py
@@ -4,7 +4,12 @@ import prettyprinter
 if __name__ == "__main__":
     src = open("source.c").read()
     t = nanoC.grammaire.parse(src)
-    # print(asm_main(t))
+    print("\n\033[107m    Source                                              \033[0m\n")
+    print(src)
+    print("\n\033[107m    Pretty Print                                        \033[0m\n")
     print(prettyprinter.pp_main(t))
+    print("\n\033[107m    Assembleur                                          \033[0m\n")
+    print(nanoC.asm_main(t))
     with open("resultat.asm", "w") as f:
         f.write(nanoC.asm_main(t))
+    print("\n\033[107m    Retour                                              \033[0m\n")
